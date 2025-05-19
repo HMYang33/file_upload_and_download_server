1b41f33a6ada1381b8c405da6cd287ca8618c869
hemengyang <HMYang33@qq.com>
2025-05-19 17:35:44 +0800

    更新程序结构，打包逻辑

--- build_run.js
+++ build_run.js
@@ -1,30 +1,51 @@
 import { execSync } from 'child_process'
 import { statSync, readFileSync, writeFileSync, mkdirSync, existsSync } from 'fs'
 let entry = './index.js'
-let outfilepath = './dist/mycli.exe'
+let outfileName = 'file_upload_and_download_server'
+let outfilepath = platform => './dist/' + outfileName + platform
 let prevSize = 0;
 let currSize = 1;
-let buildResult = {}
+let buildCmdMaker = (target, outfile) => `bun build --compile --minify --target=${target} ${entry} --outfile ${outfile}`
+let buildWorks = [
+ ["bun-linux-x64", "-linux-x64"],
+ ["bun-linux-arm64", "-linux-arm64"],
+ ["bun-windows-x64", "-windows-x64.exe"],
+ ["bun-darwin-x64", "-darwin-x64"],
+ ["bun-darwin-arm64", "-darwin-arm64"]
+]
 
-existsSync('./dist') || mkdirSync('./dist');
-
-while (prevSize !== currSize) {
- buildResult = build(entry)
- prevSize = currSize
- currSize = buildResult.size
- injectFileSizeToCode(entry, currSize)
-}
-execSync(`mycli.exe ${process.argv[2] || ''}`, { stdio: 'inherit',cwd: './dist'})
-
-function build(file) {
- execSync(`bun build ${file} --outfile ${outfilepath} --compile`)
- return {
-  outfilepath,
-  size: statSync('./dist/mycli.exe').size
+main()
+function main() {
+ existsSync('./dist') || mkdirSync('./dist');
+ for (let i = 0; i < buildWorks.length; i++) {
+  console.log("开始构建" + buildWorks[i][1] + "版本")
+  let [target, outfile] = buildWorks[i];
+  prevSize = 0;
+  currSize = 1;
+  buildForOnePlatform(target, outfile)
  }
 }
-function injectFileSizeToCode(codeFilePath, size) {
- let code = readFileSync(codeFilePath).toString().split('\n')
- code[0] = `const file_size = ${size}; // 第一行代码将在执行打包时自动修改，不要手动修改`
- writeFileSync(codeFilePath, code.join('\n'))
+
+
+function buildForOnePlatform(target, outfile) {
+ while (prevSize !== currSize) {
+  let { size } = build(target, outfilepath(outfile))
+  prevSize = currSize
+  currSize = size
+  injectFileSizeToCode(entry, currSize)
+ }
+
+ function build(target, outfile) {
+  execSync(buildCmdMaker(target, outfile), { stdio: 'inherit' })
+  return {
+   outfilepath,
+   size: statSync(outfile).size
+  }
+ }
+ function injectFileSizeToCode(codeFilePath, size) {
+  let code = readFileSync(codeFilePath).toString().split('\n')
+  code[0] = `const file_size = ${size}; // 第一行代码将在执行打包时自动修改，不要手动修改`
+  writeFileSync(codeFilePath, code.join('\n'))
+ }
 }
+
--- index.js
+++ index.js
@@ -1,64 +1,98 @@
-const file_size = 115051925; // 第一行代码将在执行打包时自动修改，不要手动修改
+const file_size = 57702384; // 第一行代码将在执行打包时自动修改，不要手动修改
 const fs = require("fs");
-
-// let isDev = process.argv[0].endsWith("bun.exe") && process.argv[1].endsWith("index.js")
-// if (isDev) {
-//  console.log("当前是开发环境启动，已跳过代码自检");
-//  console.log("正在使用 /dist/uploads 文件夹作为上传文件存储");
-//  console.log("正在使用 3000 端口作为服务端口");
-//  main();
-// } else {
-//  check()
-//  main()
-// }
-let binaryFileName = process.argv[1].split("/").pop();
-console.log(`
-  这是一个何梦洋做的静态文件资源托管服务，支持单文件上传、下载和删除。
-  启动方法：
-
-  1. 运行 ./${binaryFileName} + 空格 + 端口号
-  示例:
-  $ ./${binaryFileName} 8080
-`);
-
-let server_port = process.argv[2];
-if (isNaN(server_port)) {
- console.log("请指定端口号");
- process.exit(0);
-}
-
-let all_files_in_cwd = fs.readdirSync("./");
-let expect = [binaryFileName, "uploads"];
-if (JSON.stringify(all_files_in_cwd) !== JSON.stringify(expect)) {
- console.log("请确保执行命令时所在目录和二进制文件目录一致");
-}
-if (all_files_in_cwd.length !== expect.length) {
- console.log("请确保执行命令时所在目录只有此二进制文件，最多包含一个uploads文件夹");
-}
-let file_size_correct = file_size - 0 === fs.statSync("./" + binaryFileName).size;
-if (file_size_correct) {
- console.clear();
+var tcpPortUsed = require('tcp-port-used');
+let config = {}
+let isDev = process.argv[0].endsWith("bun.exe") && process.argv[1].endsWith("index.js")
+config.isDev = isDev
+if (isDev) {
+ config.fileHandlerDirectory = "/dist/uploads"
+ config.server_port = 0
+ console.log("当前是开发环境启动，已跳过代码自检");
+ console.log("即将使用 " + config.fileHandlerDirectory + " 文件夹作为上传文件存储");
+ console.log("即将使用 \'自动分配\' 端口作为服务端口");
  main();
 } else {
- console.log("代码自检失败，程序有可能已经被篡改");
+ config.fileHandlerDirectory = "/uploads"
+ config.binaryFileName = process.argv0.split("\\").pop();
+ config.server_port = process.argv[2] || 0;
+ console.log(`
+  这是一个何梦洋做的静态文件资源托管服务，支持单文件上传、下载和删除。
+
+启动方法：
+
+ 1. 自动分配端口模式
+直接运行: ./${config.binaryFileName}
+这将自动分配端口
+
+ 2. 指定端口号模式
+说明: ./${config.binaryFileName} + 空格 + 端口号
+使用: ./${config.binaryFileName} 3000
+`);
+ let checkResult = await checkPort() && await checkEnv()
+ if (checkResult) {
+  console.log("程序自检成功，进入主程序逻辑");
+  main()
+ }
 }
 
-function main() {
- console.log("代码自检成功，进入主程序逻辑");
+async function checkPort() {
+ if (isNaN(config.server_port)) {
+  console.log("你指定的端口号不是一个数字");
+  return false
+ } else {
+  config.server_port = parseInt(config.server_port)
+  // config.server_port should between 0 and 65535
+  if (config.server_port < 0 || config.server_port > 65535) {
+   console.log("端口号不正确，应该在0到65535之间");
+   return false
+  }
+  let inUse = await tcpPortUsed.check(config.server_port)
+  if (inUse) {
+   console.log("端口" + config.server_port + "已经被占用");
+   return false
+  } else {
+   if (config.server_port === 0) {
+    console.log("即将使用 \'自动分配\' 端口作为服务端口");
+   } else {
+    console.log("即将使用 " + config.server_port + " 端口作为服务端口");
+   }
+   return true
+  }
+ }
+}
 
- if (!fs.existsSync("./uploads")) {
-  fs.mkdirSync("./uploads");
-  console.log("您似乎是第一次使用，已为您创建uploads文件夹，上传的文件将存放于此文件夹");
+async function checkEnv() {
+ let all_files_in_cwd = fs.readdirSync("./");
+ if (all_files_in_cwd.includes(config.binaryFileName)) {
+  if (file_size === fs.statSync("./" + config.binaryFileName).size) {
+   return true
+  } else {
+   console.log("当前执行命令时所在目录确实有" + config.binaryFileName + "文件，但是它们只是文件名一样，并不是同一个文件");
+   console.log("提示：程序文件位置在:" + process.argv0);
+   return false
+  }
+ } else {
+  console.log("请确保执行命令时所在目录和本程序文件所在的目录一样");
+  return false
+ }
+}
+
+
+function main() {
+
+ if (!fs.existsSync(config.fileHandlerDirectory)) {
+  fs.mkdirSync(config.fileHandlerDirectory, { recursive: true });
+  console.log("您似乎是第一次使用，已为您创建" + config.fileHandlerDirectory + "文件夹，上传的文件将存放于此文件夹");
  }
 
  let server = Bun.serve({
-  port: server_port,
+  port: config.server_port,
   routes: {
    "/**": {
     GET: (req) => {
      const url = new URL(req.url);
-     if (fs.existsSync("./uploads" + url.pathname)) {
-      return new Response(Bun.file("./uploads" + url.pathname));
+     if (fs.existsSync(config.fileHandlerDirectory + url.pathname)) {
+      return new Response(Bun.file(config.fileHandlerDirectory + url.pathname));
      } else {
       return new Response("无文件", { status: 404 });
      }
@@ -81,8 +115,8 @@ function main() {
     DELETE: (req) => {
      const url = new URL(req.url);
      let filename = url.searchParams.get("md5");
-     if (fs.existsSync("./uploads" + filename)) {
-      fs.unlinkSync("./uploads" + filename);
+     if (fs.existsSync(config.fileHandlerDirectory + filename)) {
+      fs.unlinkSync(config.fileHandlerDirectory + filename);
       return new Response("删除成功", { status: 200 });
      } else {
       return new Response("无文件", { status: 404 });
@@ -99,9 +133,9 @@ function main() {
  if (server) {
   console.log(`启动成功 使用方法：
 
-    获取文件 GET http://你的服务器ip或localhost:${server_port}/\${md5}
-    上传文件 POST http://你的服务器ip或localhost:${server_port}/任意路径 body传一个表单对象
-    删除文件 DELETE http://你的服务器ip或localhost:${server_port}/任意路径?md5=\${md5}
+    获取文件 GET http://localhost:${server.port}/\${md5}
+    上传文件 POST http://localhost:${server.port}/任意路径    body传一个表单对象
+    删除文件 DELETE http://localhost:${server.port}/任意路径?md5=\${md5}
     `);
  }
 }
